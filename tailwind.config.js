fa753bf6bfcb012c21a1f93f2cfd3fe9567a6456
lmnhd <cc.lemonhead@gmail.com>
2022-10-23 14:31:01 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        
+       
+      },
+    },
+  },
+  plugins: [],
+}
